feb079cf80e43823299ba5c34c0dc51e1419d337
Siddarth Senthilkumar <ssenthilkumar3@gatech.edu>
2016-07-08 12:43:48 +0200

    Recreated Home page. Created login page that posts login and registration information to python appliation.

--- app/static/js/init.js
+++ app/static/js/init.js
@@ -3,6 +3,7 @@
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
+	$('.slider').slider({full_width: true});
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
--- app/views.py
+++ app/views.py
@@ -1,4 +1,6 @@
 from flask import render_template
+from flask import request
+import hashlib
 from app import app
 
 @app.route('/')
@@ -10,6 +12,21 @@ def index():
 def login():
 	return render_template('login.html')
 
-@app.route('/register.html')
+@app.route('/login', methods=['POST'])
+def verify_credentials():
+	name=request.form['login_username']
+	password=hashlib.sha256(request.form['login_password'].encode('utf-8')).hexdigest()
+	return "Username: " + name + "\nPassword: " + password
+
+@app.route('/register', methods=['POST'])
 def register():
-	return render_template('register.html')
+	name=request.form['register_username']
+	password=hashlib.sha256(request.form['register_password'].encode('utf-8')).hexdigest()
+	firstname=request.form['register_firstname']
+	lastname=request.form['register_lastname']
+	email=request.form['register_email']
+	street=request.form['register_streetaddress']
+	city=request.form['register_city']
+	state=request.form['register_state']
+	zipcode=request.form['register_zip']
+	return name + " " + password + " " + firstname + " " + lastname + " " + email + " "+ street + " " + city + " " + state + " " + zipcode
